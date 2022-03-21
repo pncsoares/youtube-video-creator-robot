cf9caadc5d6bb8b7c41647a5eacb23a7f15418a2
Pedro Soares <pncsoares@gmail.com>
2022-03-21 17:47:45 +0000

    Load data structure in images robot

--- Index.js
+++ Index.js
@@ -2,10 +2,12 @@ import 'dotenv/config'
 import inputRobot from './Robots/InputRobot.js';
 import textRobot from './Robots/TextRobot.js';
 import { loadState } from './Robots/StateRobot.js';
+import imageRobot from './Robots/ImageRobot.js'
 
 async function start() {
     inputRobot();
     await textRobot();
+    await imageRobot();
 
     const content = loadState();
     console.dir(content, { depth: null });
