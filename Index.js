2010a534f5421c2dce5c731cee766c14b1badb59
Pedro Soares <pncsoares@gmail.com>
2022-03-24 08:55:41 +0000

    Initialize video robot

--- Index.js
+++ Index.js
@@ -3,11 +3,13 @@ import inputRobot from './Robots/InputRobot.js';
 import textRobot from './Robots/TextRobot.js';
 import { loadState } from './Robots/StateRobot.js';
 import imageRobot from './Robots/ImageRobot.js'
+import videoRobot from './Robots/VideoRobot.js'
 
 async function start() {
     inputRobot();
     await textRobot();
     await imageRobot();
+    await videoRobot();
 
     const content = loadState();
     console.dir(content, { depth: null });
